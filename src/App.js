48fd98e518190f6738f13a12b4bdf075b64e1185
kanghyunjeong <hhhjjj12323@gmail.com>
2019-06-05 12:37:28 +0900

    movie app map , propTypes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const movies = [
+    {
+        title: "Matrix",
+        poster: "https://assets.www.warnerbros.com/sites/default/files/movies/media/browser/Matrix_2000x3000.JPEG"
+    },
+    {
+        title: "Full Metal Jacket",
+        poster: "https://images-na.ssl-images-amazon.com/images/I/91sTE00%2Bm%2BL._SY606_.jpg"
+    },
+    {
+        title: "Oldboy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+    },
+    {
+        title: "Star Wars",
+        poster: "https://images-na.ssl-images-amazon.com/images/I/812Ww9ReZHL.jpg"
+    }
+];
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                {movies.map((movie, index) => {
+                    return (<Movie title={movie.title} poster={movie.poster} key={index}/>)
+                })}
+
+            </div>
+        )
+    }
 }
 
 export default App;
