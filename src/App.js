bc679ec7acc41592f9c66721873e6f71e41874ab
kanghyunjeong <hhhjjj12323@gmail.com>
2019-06-11 12:34:34 +0900

    movie app setState

--- src/App.js
+++ src/App.js
@@ -22,10 +22,57 @@ const movies = [
 ];
 
 class App extends Component {
+
+    // componentWillMount() {
+    //     console.log("component will mount")
+    // }
+    //
+    // componentDidMount() {
+    //     console.log("component did mount")
+    // }
+
+    state = {
+        greeting : "Hello",
+        movies : [
+            {
+                title: "Matrix",
+                poster: "https://assets.www.warnerbros.com/sites/default/files/movies/media/browser/Matrix_2000x3000.JPEG"
+            },
+            {
+                title: "Full Metal Jacket",
+                poster: "https://images-na.ssl-images-amazon.com/images/I/91sTE00%2Bm%2BL._SY606_.jpg"
+            },
+            {
+                title: "Oldboy",
+                poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+            },
+            {
+                title: "Star Wars",
+                poster: "https://images-na.ssl-images-amazon.com/images/I/812Ww9ReZHL.jpg"
+            }
+        ]
+    };
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                // ...this.state.movies,
+                movies: [
+                    {
+                        title: "트랜스아메리카",
+                        poster: "http://t1.daumcdn.net/cfile/144BED054BF94FC90D"
+                    },
+                    ...this.state.movies
+                ]
+            })
+        }, 2000)
+    }
+
     render() {
+        console.log("render")
         return (
             <div className="App">
-                {movies.map((movie, index) => {
+                {this.state.movies.map((movie, index) => {
                     return (<Movie title={movie.title} poster={movie.poster} key={index}/>)
                 })}
 
